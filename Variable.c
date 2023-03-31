e9dba709d7a283db4308b6c9e69e961fd9ae5483
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-03-31 16:27:36 +0600

    variable

--- Variable.c
+++ Variable.c
@@ -5,4 +5,35 @@ int main()
     int name = 60;
     printf("%d", name);
 
-    return 
+    return 0;
+}
+
+
+
+//02. 2 variable 
+#include <stdio.h>
+
+int main()
+{
+    int number1 = 89;               // set variable
+    printf("Number1: %d", number1); // print vraiable
+
+    int number2 = 50;                 // set variable
+    printf("\nNumber2: %d", number2); // print vriable
+
+    return 0;
+}
+
+// 03. variable add a new variable 
+#include <stdio.h>
+
+int main()
+{
+    int firstNumber = 1;                    // set variable
+    printf("Firstnumber: %d", firstNumber); // print firstNumber
+
+    int secoundnumber = firstNumber;
+    printf("\nSecoundNumber: %d", secoundnumber);
+
+    return 0;
+}
