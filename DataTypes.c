74486c5f7d277f5166516c67333fb2358857f3ff
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-04-01 16:18:45 +0600

    data type

--- DataTypes.c
+++ DataTypes.c
@@ -1,1 +1,40 @@
-// 
+//01 int data
+#include <stdio.h>
+int main()
+{
+    // Write C code here
+    int num = 6;
+    printf("%d", num);
+
+    return 0;
+}
+
+
+//02 + 03 ..float and double
+int main()
+{
+    // Write C code here
+
+    double number1 = 70.30;  // double number variable
+    float number2 = 10.9f;   // float number vaiable
+    printf("%If", number1);  // print number
+    printf("\n%f", number2); // print data
+
+    printf("\n%.2f", number2); // print data with slice
+
+    return 0;
+}
+
+
+//04. character data
+#include <stdio.h>
+
+int main()
+{
+    // Write C code here
+
+    char name = 'yy';
+    printf("%c", name);
+
+    return 0;
+}
