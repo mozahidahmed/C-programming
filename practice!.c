151ea2a005e0e8d4cc89f152d35d49aafbcde819
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-04-05 00:19:40 +0600

    input from user

---
+++ practice!.c
@@ -0,0 +1,28 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+{
+
+    int number = 60;
+    printf("Number: %d", number);
+
+    printf("\n");
+
+    double numberd = 70.70;
+    printf("double nymber : %If", numberd);
+
+    printf("\n");
+
+    char name = 'm';
+    printf("character data: %c", name);
+
+    printf("\n");
+
+    // multple variable
+    int mnumber = 50;
+    double dnumber = 70.12;
+    printf("inteser Number %d,\n souble number: %If", mnumber, dnumber);
+
+    return 0;
+}
