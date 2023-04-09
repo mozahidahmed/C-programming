bd158f44c653550200e856431f69cbe432c3788f
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-04-09 22:31:45 +0600

    add

--- chapter2/while._loop.C
+++ chapter2/while._loop.C
@@ -4,3 +4,19 @@ for (i<=5){
     printf("%d",int)
     i++
 }
+
+
+
+//multple counter
+
+int number;
+printf("Enter a number for count:");
+scanf("%d", &number);
+//   printf("%d",number); print input value
+int count = 1;
+while (count <= 10)
+{
+    int result = number * count;
+    printf("\n%d", result);
+    count++;
+}
